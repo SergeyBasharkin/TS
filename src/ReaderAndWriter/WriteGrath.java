5bfb22bc244437a44b992c72dd7c3cbe4fab0005
RustamSafin <last.safin@yandex.ru>
2016-05-09 14:45:21 +0300

    add pushRelabel

--- src/ReaderAndWriter/WriteGrath.java
+++ src/ReaderAndWriter/WriteGrath.java
@@ -10,8 +10,8 @@ import java.util.ArrayList;
  */
 public class WriteGrath {
     public  static void main(String[] args) {
-        //TODO путь к файлу, куда записывать граф
-        File file= new File("docs/5000.txt");
+        //TODO путь к файлу, куда записывать граф 100 500 1.000 4.000 8.000 12.000 20.000 50.000 100.000 250.000 500.000
+        File file= new File("docs/500000.txt");
         if (!file.exists()){
             try {
                 file.createNewFile();
@@ -23,7 +23,7 @@ public class WriteGrath {
         try {
            pw=new PrintWriter(new FileOutputStream(file));
             //TODO в генераторе надо указывать исток и сток.
-            ArrayList<MyGenerator.MyEdge> edges=MyGenerator.generate2(0,5000);
+            ArrayList<MyGenerator.MyEdge> edges=MyGenerator.generate2(0,500000);
             for (MyGenerator.MyEdge edge:edges){
                 pw.write(edge.getFrom()+" "+edge.getTo()+" "+edge.getCap()+"\n");
             }
