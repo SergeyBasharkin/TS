b0d24b9de5e0255f5cca0b079cb621d007a1da1f
SergeyBasharkin <rock-n-rolllll@bk.ru>
2016-05-09 15:41:23 +0300

    dinic tests

--- src/Tests/DinicTest.java
+++ src/Tests/DinicTest.java
@@ -17,7 +17,7 @@ public class DinicTest {
     public static void main(String[] args) {
 
         //TODO файл для вывода тестов
-        File file= new File("docs/PushRelabelTest.txt");
+        File file= new File("docs/DinicTest.txt");
         if (!file.exists()){
             try {
                 file.createNewFile();
@@ -27,18 +27,17 @@ public class DinicTest {
         }
         PrintWriter pw=null;
         //TODO это(3 следующие строчки) для проталкивания предпотока
-        String path="docs/8000.txt";
-        ArrayList<MyGenerator.MyEdge> genGraph= ReadGrath.readEdges(path);
-        int cap[][] =MyGenerator.convertEdgeToCap(genGraph,8000);
+        String [] a={"docs/500.txt","docs/1000.txt","docs/10000.txt","docs/25000.txt","docs/50000.txt","docs/75000.txt","docs/100000.txt","docs/120000.txt","docs/250000.txt","docs/500000.txt"};
         try {
             pw=new PrintWriter(new FileOutputStream(file,true));
             //TODO вставьте свой алгоритм
-            long s =System.currentTimeMillis();
-            int maxFlow=pushRelabel(cap);
-            //TODO путь к файлу с графом
-            long e =System.currentTimeMillis();
-            pw.write("file: "+path+", time: "+(e-s)+"ms, maxFlow: "+maxFlow+"\n");
-
+            for (String p:a) {
+                long s = System.currentTimeMillis();
+                int maxFlow = dinic(p,Integer.parseInt(p.substring(5,p.length()-4)));
+                //TODO путь к файлу с графом
+                long e = System.currentTimeMillis();
+                pw.write("file: " + p + ", time: " + (e - s) + "ms, maxFlow: " + maxFlow + "\n");
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }finally {
@@ -46,16 +45,16 @@ public class DinicTest {
             pw.close();
         }
     }
-    public static int dinic() {
+    public static int dinic(String p,int size) {
         MaxFlowDinic maxFlowDinic=new MaxFlowDinic();
-        String path="docs/4000.txt";
+        String path=p;
         List<MyGenerator.MyEdge> genGraph= ReadGrath.readEdges(path);
         List<MaxFlowDinic.Edge>[] graph = maxFlowDinic.createGraph(genGraph.size());
         for (MyGenerator.MyEdge edge:genGraph){
             maxFlowDinic.addEdge(graph,edge.getFrom(),edge.getTo(),edge.getCap());
         }
         //TODO опять же в генерации надо дабовить сток и исток
-        return maxFlowDinic.maxFlow(graph,0,4000);
+        return maxFlowDinic.maxFlow(graph,0,size);
     }
     public static int pushRelabel(int[][] cap) {
 //        String path="docs/4000.txt";
